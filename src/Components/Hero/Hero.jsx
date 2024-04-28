b27616e61f94c9c7e939013abbad2d09f37c800d
byron00123 <omondibyron25@gmail.com>
2024-04-28 21:04:17 +0300

    complete site

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Hero.css'
+import { Link } from 'react-scroll';
+
 import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
@@ -10,7 +12,7 @@ const Hero = () => {
         <p>Our mission is to revolutionise agriculture in Kenya by employing sustainable farming
             practices to enhance food security, environmental conservation, and socioeconomic
             development</p>
-            <button className='btn'>Explore more<img src={dark_arrow} alt=""/></button>
+            <Link to ='products' smooth={true} offset={-260} duration={500} className='btn'>Explore more<img src={dark_arrow} alt=""/></Link>
       </div>
     </div>
   )
